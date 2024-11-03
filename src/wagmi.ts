880c7bf3cb649f2273ef4db19e0d0cbffd3aee62
0xRory <0x1rory@gmail.com>
2024-11-03 10:47:40 +0800

    test

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,14 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { holesky } from "wagmi/chains";
 
+console.log(
+  "process.env.NEXT_PUBLIC_REOWN_APP_NAME",
+  process.env.NEXT_PUBLIC_REOWN_APP_NAME
+);
+console.log(
+  "process.env.NEXT_PUBLIC_REOWN_PROJECT_ID",
+  process.env.NEXT_PUBLIC_REOWN_PROJECT_ID
+);
 export const config = getDefaultConfig({
   appName: process.env.NEXT_PUBLIC_REOWN_APP_NAME,
   projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID,
