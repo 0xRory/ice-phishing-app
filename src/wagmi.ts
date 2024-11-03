cf52917b242a2425b2c3b98c388fb3a921b9762d
0xRory <0x1rory@gmail.com>
2024-11-03 10:42:04 +0800

    fix: next env

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, sepolia, holesky } from "wagmi/chains";
+import { holesky } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: process.env.REOWN_APP_NAME,
-  projectId: process.env.REOWN_PROJECT_ID,
+  appName: process.env.NEXT_PUBLIC_REOWN_APP_NAME,
+  projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID,
   chains: [
     // mainnet,
     holesky,
